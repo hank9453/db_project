3a41e8f45a1ecb18491ffa77cb4090ce400dfbdf
hank <hank77880@gmail.com>
2024-10-16 00:40:36 +0800

    for cata

--- main.py
+++ main.py
@@ -8,6 +8,51 @@ app.secret_key = '527852785278'  # 設置一個密鑰來加密 session
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/admin/products')
+def products():
+    return render_template('admin/products.html')
+
+@app.route('/admin/Categories', methods=['POST'])
+def addcata():
+    data = json.loads(request.get_data())
+    con = sqlite3.connect('db.db')
+    cursor = con.cursor()
+    try :
+        cursor.execute("insert into Type (name) values (?)", (data['name'],))
+        con.commit()
+        con.close()
+        return 'success', 200
+    except Exception as e:
+        print(e)
+        return 'error', 400
+
+@app.route('/admin/Delete_Categories', methods=['POST'])
+def delcata():
+    data = json.loads(request.get_data())
+    con = sqlite3.connect('db.db')
+    cursor = con.cursor()
+    try :
+        cursor.execute("delete from Type where name=?", (data['name'],))
+        con.commit()
+        con.close()
+        return 'success', 200
+    except Exception as e:
+        print(e)
+        return 'error', 400
+
+
+@app.route('/admin/Load_Categories', methods=['GET'])
+def loadcata():
+    con = sqlite3.connect('db.db')
+    cursor = con.cursor()
+    try :
+        cursor.execute("select name from Type")
+        result = cursor.fetchall()
+        con.close()
+        return jsonify(result)
+    except Exception as e:
+        print(e)
+        return 'error', 400
 
 @app.route('/admin')
 def admin():
